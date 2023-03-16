5a66be632f025a55611ebbaec86594e1444847c2
Quamena <antoeric23@gmail.com>
2023-03-16 03:17:03 -0600

    Updated

--- R/moderation.R
+++ R/moderation.R
@@ -253,13 +253,4 @@ sim_data <- function(n = 100, b0, a0 = NULL, link.function = "logistic",
   dat
 }
 
-#compares.plot <- function()
-
-# a0=c(-2, 2, 2, 0, 1)
-# b0=c(-1, 0.5, 0.5, -1, .5, -0.5, 1, 0, .5)
-# sim_data(n = 100, b0, a0 = NULL, binary.Xs = FALSE,
-#                     sigma = 1, uniform = FALSE, c0 = 1,
-#                     link.function = "logistic", rho = 0.2,
-#                     observational = FALSE, trt.p = 0.5,
-#                     interaction = 1:2, details = FALSE) %>% head()
 
